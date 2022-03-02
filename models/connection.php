bc724fbe1415fe454ed24d8d33d3ae6fa3c1be5f
jennyBiinteli <jenny.garces@biinteli.com>
2022-03-02 15:42:22 -0500

    conexion a heroku

--- models/connection.php
+++ models/connection.php
@@ -7,9 +7,9 @@ class Connection
 	{
 
 		try {
-			$link = new PDO("mysql:host=localhost;dbname=demomarketplace
-			", "demomarketplace
-			", "DemoMarketPlace@"); //PRODUCCION
+			$link = new PDO("mysql:host=us-cdbr-east-05.cleardb.net:3306;dbname=heroku_95a8d1ce0696946
+			", "b31201012d645e
+			", "42a8ed3d0d30baa"); //PRODUCCION
 			// $link = new PDO("mysql:host=localhost:33065;dbname=marketplace", "root", ""); //LOCAL
 			$link->exec("set names utf8");
 		} catch (PDOException $e) {
