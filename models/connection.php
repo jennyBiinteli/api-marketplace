8ca22a8d24a4fec69b7f30c4a08f1b2b10c895be
jennyBiinteli <jenny.garces@biinteli.com>
2022-03-02 15:59:44 -0500

    pruebapass

--- models/connection.php
+++ models/connection.php
@@ -7,9 +7,7 @@ class Connection
 	{
 
 		try {
-			$link = new PDO("mysql:host=us-cdbr-east-05.cleardb.net:3306;dbname=heroku_95a8d1ce0696946
-			", "b31201012d645e
-			", "42a8ed3d0d30baa"); //PRODUCCION
+			$link = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_95a8d1ce0696946", "b31201012d645e", "2ce2f069"); //PRODUCCION
 			// $link = new PDO("mysql:host=localhost:33065;dbname=marketplace", "root", ""); //LOCAL
 			$link->exec("set names utf8");
 		} catch (PDOException $e) {
