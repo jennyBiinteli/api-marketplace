2c3596e70eed72bc31131a133a7967b01b79c9ca
jennyBiinteli <jenny.garces@biinteli.com>
2022-03-02 16:21:28 -0500

    prueba01

--- models/connection.php
+++ models/connection.php
@@ -5,9 +5,12 @@ class Connection
 
 	static public function connect()
 	{
+		$servidor = "localhost";
+		$usuario = "b31201012d645e";
+		$password = "42a8ed3d0d30baa";
 
 		try {
-			$link = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_95a8d1ce0696946", "b31201012d645e", "2ce2f069"); //PRODUCCION
+			$link = new PDO("mysql:host=$servidor;dbname=heroku_95a8d1ce0696946", $usuario, $password); //PRODUCCION
 			// $link = new PDO("mysql:host=localhost:33065;dbname=marketplace", "root", ""); //LOCAL
 			$link->exec("set names utf8");
 		} catch (PDOException $e) {
